73a405cb1d26c25ed2121930bceef5d2a2ced370
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-06-03 18:53:55 -0400

    menubar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'sebastian';
+
+  cambiarNombre(nombre:string) {
+    console.log(nombre);
+  }
+
 }
